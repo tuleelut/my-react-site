18c2891d4fa779c1adf04204ad5be33bab6c76eb
Rita Daukšienė <rita78@gmail.com>
2022-12-11 14:11:50 +0200

    rwerwr

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  const [error, setError] = useState(false);
+
+  function clickHandler() {
+    if(count>-1){
+      setError(false)
+    }
+    setCount(count + 1);
+  }
+  function clickHandlerMinus() {
+    if (count == 0) {
+      setError(true);
+      return;
+    }
+    setCount(count - 1);
+  }
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="./vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div>{count}</div>
+      <button onClick={clickHandler}>+1</button>
+      <button onClick={clickHandlerMinus}>-1</button>
+      {error && <div>Neigiami skaičiai negalimi</div>}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
